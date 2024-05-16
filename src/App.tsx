1366e808286d93b1c8d9ba9cc0e5ac7a4764fcde
ivan <ivandronovais@gmail.com>
2024-05-15 22:47:10 -0300

    updated app name

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ function App() {
         <>
             <Container>
                 <Header>
-                    <HeaderText>Ct-Money</HeaderText>
+                    <HeaderText>Ct-Money - Gerenciador de Despesas</HeaderText>
                 </Header>
                 <Body>
                     {/* Área de Informações */}
